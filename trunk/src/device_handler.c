fbbcb88a03fd3c7a86c653c4aa49cd3ac6fd60d0
johan.nanzen <johan.nanzen@2ae7d225-5b03-ae47-7f5e-e126e47135e4>
2010-05-05 18:47:30 +0000

    Polled Console working. Malta working. Also made some rearrangements of functions. All output is handled in Device_handler.

--- trunk/src/device_handler.c
+++ trunk/src/device_handler.c
@@ -8,13 +8,38 @@
 #include "device_handler.h"
 
 // Assignes a name to a connected devices memory allocation.
-/*
+
 void IO_device(short id, short owner, void* buffer_address){
-	device->id = id;
-	device->owner = owner;
-	device->buffer_address = buffer_address;
+	Device* d = (Device*)524288;
+	d->id = id;
+	d->owner = owner;
+	d->buffer_address = buffer_address;
 }
-*/
+
+void putWord(uint32_t word){
+  int i;
+  malta->ledbar.reg = 0xFF;
+  for (i = 7; i >= 0; --i) {
+    malta->asciipos[i].value = '0' + word % 10;
+    word /= 10;
+  }
+}
+
+void putCh(char c) {
+  // Poll until ready to transmit.
+  while ( !tty-> lsr.thre ) {}
+
+  // Write character to Transmitter Holding Register
+  tty->thr = c;
+}
+
+void putStr(const char* text) {
+  while (text[0] != '\0') {
+   putCh(text[0]);
+    ++text;
+  }
+}
+
 // Prints to stdOut and resets the buffer
 void buffer_flush(void){
 
@@ -30,3 +55,13 @@ void unblock(void){
 
 }
 */
+
+void init_devices(){
+	//Set MCR Out2 to 1 to enable interrupts on the console.
+	//tty->mcr.out2 = 1;
+
+	// Set the UART word length to 8 bits.
+	// Set word length to 8 bits (two least significant bits to 1),
+	tty->lcr.wls0 = 1;
+	tty->lcr.wls1 = 1;
+}
