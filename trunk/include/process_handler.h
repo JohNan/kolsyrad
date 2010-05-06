6b7a6e39bc4ede2639d30c3fabe67ce29d4c97a3
steffenomak <steffenomak@2ae7d225-5b03-ae47-7f5e-e126e47135e4>
2010-05-06 11:45:54 +0000

    stefan

--- trunk/include/process_handler.h
+++ trunk/include/process_handler.h
@@ -25,5 +25,11 @@ typedef struct {
 	pcb pcb_array[PROC_COUNT]; // An array of all current pcb's.
 }proc_handler;
 
+typedef struct {
+	pcb * first_ready; //a pointer to the first ready process' PCB. It will only change if the process is not longer
+	pcb * ready; //a pointer to the currently runnning process' PCB. It will walk through all process of highest current priority when scheduling.
+	pcb * waiting; //a pointer to the first waiting process' PCB.
+} pcb_queues;
+
 
 #endif
--- trunk/include/scheduler.h
+++ trunk/include/scheduler.h
@@ -3,12 +3,6 @@
 
 #include "process_handler.h"
 
-typedef struct {
-	pcb * first_ready; //a pointer to the first ready process' PCB. It will only change if the process is not longer
-	pcb * ready; //a pointer to the currently runnning process' PCB. It will walk through all process of highest current priority when scheduling.
-	pcb * waiting; //a pointer to the first waiting process' PCB.
-} pcb_queues;
-
 void schedule();
 void add_to_pcb( pcb * newProcess );
 
