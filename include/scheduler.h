abc376befe29d90697b34dc2bd64199492c2501e
steffenomak <steffenomak@2ae7d225-5b03-ae47-7f5e-e126e47135e4>
2010-05-07 12:59:00 +0000

    jadu vad ska man hitta på nu då

--- include/scheduler.h
+++ include/scheduler.h
@@ -9,29 +9,16 @@
 pcb_queues * S_PcbQueue = 0;
 free_pcb * S_FreePcb = 0;
 
-void schedule( pcb_queues * queue );
+void S_schedule( pcb_queues * queue );
 
 // Lägger till en ny pcb i pcb queues
-void add_new_pcb( pcb * toAdd, pcb_queues * queue );
-
-/* Om processen som körs har terminerat av sig själv.
- * Antar att det bara är processer som kör som någonsin
- * kan terminera, men eftersom det kan finnas möjligheten
- * så kodar jag två olika med samma namn men med olika antal
- * argument.
- */
-
-/* Denna funktion tar bårt den körande pcb, och matar den fram ett steg
- */
-void remove_pcb();
-
-/* Tar bort en specific pcb, men gör inget med den nu.
- */
-void remove_pcb( pcb * removePcb );
-
-/* initierar dom två truckterna som står längst upp
- */
+void S_add_new_pcb( pcb * toAdd, pcb_queues * queue );
 void S_set_pcb_queues( pcb_queues * queue );
 void S_set_free_pcb( free_pcb * queue );
-
+void S_remove_active();
+void S_remove_inactive( pcb * toDelete );
+// Ska alltid ligga i waiting kön
+void S_activate_pcb( pcb * toActivate );
+// Ska alltid ligga i kör kön
+void S_deactivate_pcb( pcb *toDeactivate);
 #endif
