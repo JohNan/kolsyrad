51c0e252e6d3f6309360b881c909002c6c95a6a6
bobonolla <bobonolla@2ae7d225-5b03-ae47-7f5e-e126e47135e4>
2010-05-26 10:54:50 +0000

    Nu borde schedulern funka med prio

--- src/scheduler.c
+++ src/scheduler.c
@@ -5,21 +5,27 @@ free_pcb * S_freeQ;
 
 void S_schedule(){
 	if( S_pcbQ->first_ready == NULL ){
-		//Do nothing
-	} else {
-
-		kset_registers( &( S_pcbQ->ready->registers ) );
-	/*
-		if( S_pcbQ->ready->next->priority > S_pcbQ->ready->priority ){
-			S_pcbQ->ready = S_pcbQ->ready->next;
-		} else {
-			S_pcbQ->ready = S_pcbQ->first_ready;
+		DputStr( "PiNK SCREEN\n" );
+		DputStr( "No processes to run, " );
+		while( 1 ){
+			DputStr( "rEEbOOt" );
 		}
-	 */
+	} else if( S_pcbQ->ready->priority > S_pcbQ->ready->next->priority ){
+		kset_registers( &S_pcbQ->ready->registers );
+		S_pcbQ->ready = S_pcbQ->first_ready;
+	} else {
+		kset_registers( &S_pcbQ->ready->registers );
+		S_pcbQ->ready = S_pcbQ->ready->next;
 	}
 
-	S_pcbQ->ready = S_pcbQ->ready->next;
 	kload_timer(1 * timer_msec);
+}
+
+void SP_add_before( pcb * toAdd, pcb * before ){
+	toAdd->next = currentInLoop;
+	toAdd->prev = currentInLoop->prev;
+	currentInLoop->prev->next = toAdd;
+	currentInLoop->prev = toAdd;
 }
 
 void S_add_new_pcb( pcb * toAdd ){
@@ -32,44 +38,39 @@ void S_add_new_pcb( pcb * toAdd ){
 	} else {
 		pcb * currentInLoop = S_pcbQ->first_ready;
 		if( toAdd->priority > currentInLoop->priority ){
-
-			toAdd->next = currentInLoop;
-			toAdd->prev = currentInLoop->prev;
-			currentInLoop->prev->next = toAdd;
-			currentInLoop->prev = toAdd;
+			SP_add_before( toAdd, currentInLoop );
 
 			S_pcbQ->first_ready = toAdd;
-
+			S_pcbQ->ready = toAdd;
 		} else {
 			currentInLoop = currentInLoop->next;
 
 			int i = 1;
 			while( i ){
 				if( currentInLoop == S_pcbQ->first_ready ){
-					toAdd->next = currentInLoop;
-					toAdd->prev = currentInLoop->prev;
-					currentInLoop->prev->next = toAdd;
-					currentInLoop->prev = toAdd;
-						i = 0;
+					SP_add_before( toAdd, currentInLoop );
+					i = 0;
 				} else if( toAdd->priority > currentInLoop->priority ){
-					toAdd->next = currentInLoop;
-					toAdd->prev = currentInLoop->prev;
-					currentInLoop->prev->next = toAdd;
-					currentInLoop->prev = toAdd;
-						i = 0;
-				} else {
-						i++;
-						currentInLoop = currentInLoop->next;
+					SP_add_before( toAdd, currentInLoop );
+					i = 0;
 				}
+				currentInLoop = currentInLoop->next;
 			}
 
 		}
-		/*
-		toAdd->next = S_pcbQ->ready;
-				toAdd->prev = S_pcbQ->ready->prev;
-				S_pcbQ->ready->prev->next = toAdd;
-				S_pcbQ->ready->prev = toAdd; */
+	}
+}
 
+void SP_add_to_free( pcb * q ){
+	q->prev = NULL;
+
+	if( S_freeQ->first == NULL ){
+		q->next = NULL;
+		S_freeQ->first = q;
+		S_freeQ->last = q;
+	} else {
+		q->next = S_freeQ->first;
+		S_freeQ->first = q;
 	}
 }
 
@@ -77,33 +78,27 @@ void S_remove_active(){
 	pcb * current = NULL;
 	current = S_pcbQ->ready->prev;
 
-	if( S_pcbQ->ready == S_pcbQ->ready->prev ){
-		S_pcbQ->ready->next = S_freeQ->first;
-		S_freeQ->first = S_pcbQ->ready;
+	if( S_pcbQ->ready == current ){
+		DputStr( "S_remove_active: 1\n" );
 
 		S_pcbQ->ready = NULL;
 		S_pcbQ->first_ready = NULL;
 	} else if( current == S_pcbQ->first_ready ){
+		DputStr( "S_remove_active: 2\n" );
 		current->prev->next = current->next;
 		current->next->prev = current->prev;
 
 		S_pcbQ->first_ready = current->next;
-
-		if( S_freeQ->first == NULL ){
-			current->next = NULL;
-			S_freeQ->first = current;
-		} else {
-			current->next = S_freeQ->first;
-			S_freeQ->first = current;
-		}
 	} else {
-
+		DputStr( "S_remove_active: 3\n" );
+		current->prev->next = current->next;
+		current->next->prev = current->prev;
 	}
-
+	SP_add_to_free( current );
 }
 
 pcb* getCurrent(){
-	return S_pcbQ->ready;
+	return S_pcbQ->ready->prev;
 }
 
 void init_scheduler(pcb_queues * p1, free_pcb * p2){
