3eb979acf2da248720d3b31edfbcc3a5169e7649
johan.nanzen <johan.nanzen@2ae7d225-5b03-ae47-7f5e-e126e47135e4>
2010-05-26 13:46:56 +0000

    Added input/output queues to use in kjell. Not currently working though.,,

--- include/_boot.h
+++ include/_boot.h
@@ -8,6 +8,7 @@ void syscall_putMalta(uint32_t word);
 void syscall_putStr(bounded_fifo* bfifo, char* ch);
 void syscall_putC(bounded_fifo* bfifo, char ch);
 uint8_t syscall_getC();
+char *syscall_getS();
 void syscall_exec(pib *new);
 pcb *syscall_fork();
 void syscall_set_prio(pcb *who, int prio);
