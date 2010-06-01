a8a5f993f689bae4fdad3d1b105d4e88f68c5541
steffenomak <steffenomak@2ae7d225-5b03-ae47-7f5e-e126e47135e4>
2010-06-01 10:57:33 +0000

    fiskar

--- include/init.h
+++ include/init.h
@@ -14,6 +14,19 @@
 #include "interrupt_handler.h"
 #include "_boot.h"
 
+/* init
+ * TYPES: none -> none
+ * PRE: none
+ * POST: none
+ * SIDE-EFFECT: It initates all modules that are needed
+ */
 void init();
+
+/* ksyscall_handler( registers* reg )
+ * TYPES: registers_t* -> none
+ * PRE: the pointer must be a valid one
+ * POST: none
+ * SIDE_EFFECT: loads all register from the address reg
+ */
 void ksyscall_handler(registers_t* reg);
 #endif
