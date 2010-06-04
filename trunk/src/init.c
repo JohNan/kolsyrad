71ef95fdf97fa73ccd50f2fef257f2ac35d83bbd
steffenomak <steffenomak@2ae7d225-5b03-ae47-7f5e-e126e47135e4>
2010-06-04 11:32:13 +0000

    lite opt i init.c

--- trunk/src/init.c
+++ trunk/src/init.c
@@ -7,59 +7,59 @@
  * SIDE-EFFECT: prints the world's coolest splash screen
  */
 void bootscr(){
-	DputStr("                                   ooo ");
-	DputStr("                                  o   o");
-	DputStr("         ooooo                    o   o");
-	DputStr("        o     o                   o   o                        oooo");
-	DputStr("       o       o                   ooo                        o    o ");
-	DputStr("      o         o                                            o      o ");
-	DputStr("      o         o                                           o        o ");
-	DputStr("      o         o                                           o        o  ");
-	DputStr("      o         o                            oooo           o        o  ");
-	DputStr("      o         o                           o    o          o        o   ");
-	DputStr("       o       o                           o      o          o      o 	");
-	DputStr("        o     o          oooo              o      o           o    o ");
-	DputStr("         ooooo          o    o             o      o            oooo  ");
-	DputStr("                       o      o            o      o  ");
-	DputStr("                      o        o            o    o ");
-	DputStr("                      o        o             oooo   ");
-	DputStr("                      o        o   	");
-	DputStr("                      o        o 	");
-	DputStr("                       o      o 	");
-	DputStr("                        o    o   ");
-	DputStr("     ooo                 oooo  ");
-	DputStr("    o   o                              ooo   	");
-	DputStr("    o   o        oooo                 o   o                   ooo   ");
-	DputStr("    o   o       o    o                o   o                  o   o  ");
-	DputStr("     ooo       o      o               o   o                  o   o  ");
-	DputStr("               o      o                ooo                   o   o           	");
-	DputStr("               o      o                                       ooo            	");
-	DputStr("               o      o                       ooo                            	");
-	DputStr("                o    o                       o   o                           	");
-	DputStr("                 oooo                       o     o                          	");
-	DputStr("                            ooo             o     o                          	");
-	DputStr("                           o   o             o   o                           	");
-	DputStr("                           o   o              ooo              ooo           	");
-	DputStr("                           o   o                              o   o          	");
-	DputStr("          ooo               ooo                               o   o          	");
-	DputStr("         o   o                                                o   o          	");
-	DputStr("        o     o                                                ooo           	");
-	DputStr("        o     o                                                              	");
-	DputStr("        o     o                                                              	");
-	DputStr("         o   o                                                               	");
-	DputStr("          ooo                           oo                                   	");
-	DputStr("                                       o  o            oo                    	");
-	DputStr("                       ooo             o  o           o  o                   	");
-	DputStr("                      o   o             oo           o    o                  	");
-	DputStr("                     o     o                         o    o                  	");
-	DputStr("                     o     o   CARBONATED            o    o                  	");
-	DputStr("                     o     o   - A bubbling OS        o  o           oo      	");
-	DputStr("                      o   o                            oo           o  o     	");
-	DputStr("                       ooo                                          o  o     	");
-	DputStr("                                                                     oo      	");
-	DputStr("                                                                             	");
-	DputStr("  By: Johan Nenzén, Per Boström, Stefan Risberg, Krister Emrén - 2010 ");
-	DputStr("");
+	DputStr("                                   ooo \n");
+	DputStr("                                  o   o\n");
+	DputStr("         ooooo                    o   o\n");
+	DputStr("        o     o                   o   o                        oooo\n");
+	DputStr("       o       o                   ooo                        o    o\n");
+	DputStr("      o         o                                            o      o\n");
+	DputStr("      o         o                                           o        o\n");
+	DputStr("      o         o                                           o        o\n");
+	DputStr("      o         o                            oooo           o        o\n");
+	DputStr("      o         o                           o    o          o        o\n");
+	DputStr("       o       o                           o      o          o      o\n");
+	DputStr("        o     o          oooo              o      o           o    o\n");
+	DputStr("         ooooo          o    o             o      o            oooo\n");
+	DputStr("                       o      o            o      o\n");
+	DputStr("                      o        o            o    o\n");
+	DputStr("                      o        o             oooo\n");
+	DputStr("                      o        o\n");
+	DputStr("                      o        o\n");
+	DputStr("                       o      o\n");
+	DputStr("                        o    o\n");
+	DputStr("     ooo                 oooo\n");
+	DputStr("    o   o                              ooo\n");
+	DputStr("    o   o        oooo                 o   o                   ooo\n");
+	DputStr("    o   o       o    o                o   o                  o   o\n");
+	DputStr("     ooo       o      o               o   o                  o   o\n");
+	DputStr("               o      o                ooo                   o   o\n");
+	DputStr("               o      o                                       ooo\n");
+	DputStr("               o      o                       ooo\n");
+	DputStr("                o    o                       o   o\n");
+	DputStr("                 oooo                       o     o\n");
+	DputStr("                            ooo             o     o\n");
+	DputStr("                           o   o             o   o\n");
+	DputStr("                           o   o              ooo              ooo\n");
+	DputStr("                           o   o                              o   o\n");
+	DputStr("          ooo               ooo                               o   o\n");
+	DputStr("         o   o                                                o   o\n");
+	DputStr("        o     o                                                ooo\n");
+	DputStr("        o     o\n");
+	DputStr("        o     o\n");
+	DputStr("         o   o\n");
+	DputStr("          ooo                           oo\n");
+	DputStr("                                       o  o            oo\n");
+	DputStr("                       ooo             o  o           o  o\n");
+	DputStr("                      o   o             oo           o    o\n");
+	DputStr("                     o     o                         o    o\n");
+	DputStr("                     o     o   CARBONATED            o    o\n");
+	DputStr("                     o     o   - A bubbling OS        o  o           oo\n");
+	DputStr("                      o   o                            oo           o  o\n");
+	DputStr("                       ooo                                          o  o\n");
+	DputStr("                                                                     oo\n");
+	DputStr("\n");
+	DputStr("  By: Johan Nenzén, Per Boström, Stefan Risberg, Krister Emrén - 2010\n");
+	DputStr("\n");
 }
 
 /* init
@@ -83,64 +83,13 @@ void init() {
 	//init exceptions
 	init_exc();
 
-	//Starts the idle process
-	//i = make_process(0,1,0);
-	//Starts kjell
-//	i = make_process(2,2,0);
-
-
 	//Start idle process
-	i = make_process(0,1,0);
+	i = make_process(0,1,NULL);
 	//Start kjell
-	i = make_process(2,2,0);
-	//Start malta scroll
-	//i = make_process(1,25,0);
+	i = make_process(2,2,NULL);
+	//Start malta
+	i = make_process(1,25,NULL);
 
-/*
-		i = make_process(3,30,20);
-		if(i != -1){
-		DputStr("Forth process created.");
-
-	}
-	i = make_process(4,25,20);
-		if(i != -1){
-		DputStr("Fifth process created.");
-
-		}
-
-
-
-	char tmp[8];
-	tmp[8] = '\0';
-
-	char tmp2[8];
-	tmp2[8] = '\0';
-
-
-	for(i=0;i<2;i++){
-		DputStr("---Process---");
-		DputStr("PID: ");
-		printPid(&pcbs[i]);
-		DputStr("Name: ");
-		DputStr(pcbs[i].progid->pname);
-		DputStr("Next: ");
-		printPid(pcbs[i].next);
-		DputStr("Prev: ");
-		printPid(pcbs[i].prev);
-		DputStr("Prio: ");
-		printPrio(&pcbs[i]);
-		DputStr("---end---");
-	}
-
-	DputStr("---First---");
-	printPid(readyQ.first);
-	DputStr("---Next---");
-	printPid(readyQ.first->next);
-*/
-
-	// now we just wait for an exception to occur and start scheduling
-
-//	DputStr("Init done.");
 	bootscr();
 	enableTimer();
 
